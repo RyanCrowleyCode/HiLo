079ab8edc4ddca0813b77fdbf2891869d80b50aa
Ryan Crowley <ryancrowleycode@gmail.com>
2020-06-16 16:46:37 -0500

    first commit

---
+++ src/HiLo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String playAgain = "";
+		int numOfTries = 0;
+
+		do {
+			// Create a random number for the user to guess
+			int theNumber = (int)(Math.random() * 1000 + 1);
+			// System.out.println( theNumber) ;
+			int guess = 0;
+
+			while (guess != theNumber) {
+				System.out.println("Guess a number between 1 and 1000:");
+				guess = scan.nextInt();
+				numOfTries ++;
+
+				if (guess < theNumber) {
+					System.out.println(guess + " is too low. Try again.");
+				} else if (guess > theNumber) {
+					System.out.println(guess + " is too high. Try again.");
+				} else {
+					System.out.println(guess + " is correct. You win!");
+					System.out.println("It took you " + numOfTries + " guesses to win.");
+				}
+			}
+			System.out.println("Would you like to play again (y/n)?");
+			playAgain = scan.next();
+		} while (playAgain.equalsIgnoreCase("y"));
+		System.out.println("Thank you for playing! Goodbye.");
+		scan.close();
+	}
+
+}
